dd1402628822aaae451f39fb973f8db40877490d
noir <29525767+neirenoir@users.noreply.github.com>
2025-08-01 14:22:51 +0200

    Fixed relative routes in nav

--- pages/main.ts
+++ pages/main.ts
@@ -13,9 +13,9 @@ export function neiredevPage(main: HTMLStream[]) {
         <nav>
           <span class="logo"><a href="/">neire.dev</a></span>
           <ul>
-            <li><a href="/about">About Me</a></li>
-            <li><a href="/experience">Experience</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
+            <li><a href="about">About Me</a></li>
+            <li><a href="experience">Experience</a></li>
+            <li><a href="portfolio">Portfolio</a></li>
             <!--<li><a href="/demos">Demos</a></li>-->
           </ul>
           <a href="mailto:contact@neire.dev">Contact</a>
